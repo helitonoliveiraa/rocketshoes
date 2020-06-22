faf985cc0790041eac5b9ad8b7bf3c7ef1304b5b
Heliton Oliveira <heliton.oliveira88@gmail.com>
2020-06-22 20:37:41 -0300

    changed to use hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,8 @@ module.exports = {
   },
   "plugins": [
     "react",
-    "prettier"
+    "prettier",
+    "react-hooks",
   ],
   "rules": {
     "prettier/prettier": "error",
@@ -32,5 +33,7 @@ module.exports = {
     "import/prefer-default-export": "off",
     "no-param-reassign": "off",
     "no-console": ["error", { allow: ["tron"] }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
   }
 };
